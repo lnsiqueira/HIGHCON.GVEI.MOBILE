29483ad72f0a2337a47ee988e7ced264a11fcd62
Lucas <Luciano@DESKTOP-SM3OUHP>
2020-01-26 20:21:39 +0100

    Projeto inicial

---
+++ HIGHCON.GVEI.MOBILE/HIGHCON.GVEI.MOBILE/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using HIGHCON.GVEI.MOBILE.Views;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HIGHCON.GVEI.MOBILE
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+   // [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PopupNavigation.PushAsync(new PopUp());
+        }
+ 
+        private void OpenPopup(object sender, EventArgs e)
+        {
+            PopupNavigation.PushAsync(new PopUp());
+        }
+
+        private void ClickedOk(object sender, EventArgs e)
+        {
+          
+            //NavigationPage nav = new NavigationPage(new CarPage());
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
---
+++ HIGHCON.GVEI.MOBILE/HIGHCON.GVEI.MOBILE/PopUp.xaml.cs
@@ -0,0 +1,59 @@
+﻿using HIGHCON.GVEI.MOBILE.Views;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HIGHCON.GVEI.MOBILE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopUp 
+    {
+        public PopUp()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //PopUpNavigation.Instance.PopAsync(true);
+
+            string _user = user.Text;
+            string password = senha.Text;
+
+            if (_user != "lucas")
+            {
+                //"usuario invalido"
+                return;
+            }
+
+
+           ; 
+            
+            if (password != "x")
+            {
+                //"senha invalida"
+                return;
+            }
+
+
+
+
+
+            //Chama tela HomePage
+
+            //new NavigationPage(new CarPage());
+            //Navigation.PushAsync(new CarPage());
+            PopupNavigation.Instance.PopAsync();
+
+            Navigation.PushAsync(new HomePage());
+
+
+
+        }
+    }
+}
